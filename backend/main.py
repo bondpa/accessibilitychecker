9a0f3c5c591d849a9761d379bb63938fe547df36
Backa Elias Bondpä <backaeliasbondpa@gmail.com>
2025-03-16 20:40:35 +0100

    Add endpoint for wcag check of single webpage

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pagechecker import check_wcag_accessibility
 
 app = FastAPI()
 
@@ -6,3 +7,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/check-webpage/")
+async def check_webpage(url: str):
+    # print(url)
+    # url = 'https://www.w3.org/WAI/demos/bad/before/home.html'
+    issues = check_wcag_accessibility(url)
+    return {"message": "Checking webpage for accessibility issues", "url": url, "issues": issues}
